f0ee51a024d12e4fd09b19a3591268fe7121cbf5
gulaa <gula@pr.km.ua>
2023-01-03 17:24:28 +0200

    forgot some files...

--- fpdf_learn.py
+++ fpdf_learn.py
@@ -1,21 +1,38 @@
 from fpdf import FPDF
 
-headers = ['product_id', 'product_name'] #,'amount_purchased','price_per_unit','total_price']
-
+headers = ['Product ID', 'Product Name', 'Amount',
+           'Price per Unit', 'Total Price']
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 
 pdf.add_page()
-pdf.set_font('Arial','B',16)
+pdf.set_font('Arial','B',12)
 
-top = pdf.y
-offset = pdf.x + 50
+# top = pdf.y
+# offset = pdf.x + 50
 
-for header in headers:
+# for header in headers:
 # offset = pdf.x + 50
 # print(top, offset)
-    pdf.multi_cell(50,10,header,1,0)
+# print(pdf.y, pdf.x)
+top = pdf.y
+offset = pdf.x 
+cell_w = 35
+i = 1
+for header in headers:
+    pdf.multi_cell(w=cell_w, h=10, txt=header, border=1, align='L')
+# print(pdf.y, pdf.x)
+    step = cell_w * i
     pdf.y = top
-    pdf.x = offset 
+    pdf.x = offset + step
+    i += 1
+# print(pdf.y, pdf.x)
+# pdf.multi_cell(w=cell_w, h=10, txt=headers[1], border=1, align='L')
+# print(pdf.y, pdf.x)
+# pdf.y = top
+# pdf.x = offset + 100
+# pdf.multi_cell(w=cell_w, h=10, txt=headers[2], border=1, align='L')
+# pdf.multi_cell(w=50, h=10, txt=headers[1], border=1, align='L')
+# print(pdf.y, pdf.x)
 # print(top, offset)
 # pdf.multi_cell(100,10,headers[1],1,0)
 
